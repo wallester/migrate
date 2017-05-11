938352e229ea753b8ab9558107b38bcb79485635
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-05-12 01:50:47 +0300

    Implement 'down' command

--- command/down.go
+++ command/down.go
@@ -1,10 +1,15 @@
 package command
 
 import (
+	"github.com/juju/errors"
 	"github.com/urfave/cli"
 )
 
 // Down migrates down
 func Down(c *cli.Context) error {
+	if err := migrate(c, false); err != nil {
+		return errors.Annotate(err, "migrating down failed")
+	}
+
 	return nil
 }
