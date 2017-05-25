70c8f94c29c7941fbb06cfe01d16f3717f96687d
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-05-25 11:56:09 +0300

    Rollback transaction using a function

--- driver/postgres/driver.go
+++ driver/postgres/driver.go
@@ -96,30 +96,26 @@ func (db *postgres) ApplyMigrations(ctx context.Context, files []file.File, up b
 		return errors.Annotate(err, "starting database transaction failed")
 	}
 
+	rollback := func(reasonErr error) error {
+		if err := tx.Rollback(); err != nil {
+			return errors.Annotate(err, "rolling back transaction failed")
+		}
+
+		return reasonErr
+	}
+
 	for _, file := range files {
 		if _, err := tx.ExecContext(ctx, file.SQL); err != nil {
-			if rollbackErr := tx.Rollback(); rollbackErr != nil {
-				return errors.Annotate(err, "rolling back transaction failed")
-			}
-
-			return errors.Annotate(err, "executing migration failed")
+			return rollback(errors.Annotate(err, "executing migration failed"))
 		}
 
 		if _, err := tx.ExecContext(ctx, applyMigrationSQL[up], file.Version); err != nil {
-			if rollbackErr := tx.Rollback(); rollbackErr != nil {
-				return errors.Annotate(err, "rolling back transaction failed")
-			}
-
-			return errors.Annotate(err, "executing migration failed")
+			return rollback(errors.Annotate(err, "executing migration failed"))
 		}
 	}
 
 	if err := tx.Commit(); err != nil {
-		if rollbackErr := tx.Rollback(); rollbackErr != nil {
-			return errors.Annotate(err, "rolling back transaction failed")
-		}
-
-		return errors.Annotate(err, "committing migrations failed")
+		return rollback(errors.Annotate(err, "committing migrations failed"))
 	}
 
 	return nil
